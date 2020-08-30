db286b6c7cff249fb2f5a308a16a83d9574fab88
Vyacheslav Goryachin <gor.vya4eslav@yandex.ru>
2020-08-31 02:24:11 +0300

    add enzyme test, create new test

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
 
+// it('renders without crashing', () => {
+//     const div = document.createElement('div');
+//     ReactDOM.render(<Provider store={store}>
+//         <App />
+//     </Provider>, div);
+//     ReactDOM.unmountComponentAtNode(div);
+// });
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
 it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Provider store={store}>
-        {/* <App /> */}
-    </Provider>, div);
-    ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
 });
